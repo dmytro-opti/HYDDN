1aac78d279ac6f56b0383c0c34ff2865840cadd2
mashk <mashkara320@gmail.com>
2026-04-30 21:26:31 +0300

    update BookingModel.cs and added BookingStatus.cs

--- TravellerAI.Domain/Models/BookingModel.cs
+++ TravellerAI.Domain/Models/BookingModel.cs
@@ -1,6 +1,26 @@
+using DateTime = System.DateTime;
+using Guid = System.Guid;
+using TravellerAI.Domain.Enums;
+
+
 namespace TravellerAI.Domain.Models;
 
-public class BookingModel
-{
-    
-}
+    public class BookingModel
+    {
+        public Guid BookingId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PropertyId { get; set; }
+        public Guid RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Currency { get; set; } 
+        public bool IsPaid { get; set; }
+        public string PaymentMethod { get; set; }
+        public int GuestCount { get; set; }
+        public BookingStatus Status { get; set; } 
+        public BudgetModel Budget { get; set; }
+    }
+
+
