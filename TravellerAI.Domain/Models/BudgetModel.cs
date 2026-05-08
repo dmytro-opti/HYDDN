fcd56b06e9b93f65c09f2627a6bfdde6b8edf145
diana <diana.kov302@gmail.com>
2026-05-08 20:11:03 +0300

    Add BudgetModel UserInfoModel

--- TravellerAI.Domain/Models/BudgetModel.cs
+++ TravellerAI.Domain/Models/BudgetModel.cs
@@ -1,6 +1,12 @@
 namespace TravellerAI.Domain.Models;
-
+// фільтр
 public class BudgetModel
 {
-    
+    public RecommendationModel Recommendation { get; set; }
+    public decimal Budget { get; set; } //Limit
+    public decimal Total { get; set; }
+    public string CurrencyCode { get; set; } = "USD";
+    public bool IsWithBudget => Total <= Budget;
+    public decimal RemainingBudget => Budget - Total;
+    public BalanceModel Ballance { get; set; }
 }
