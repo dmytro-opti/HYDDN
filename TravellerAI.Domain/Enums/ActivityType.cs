a9557814efec83ebecaabd0831c6c2fc90a54636
yaroslavkhmil <yaroslavhmel2008@gmail.com>
2026-05-09 09:30:19 +0300

    add ActivityType enum with ENTERTAINMENT, EDUCATION, SPORTS, TRAVEL, OTHER

---
+++ TravellerAI.Domain/Enums/ActivityType.cs
@@ -0,0 +1,10 @@
+namespace TravellerAI.Domain.Enums;
+
+public enum ActivityType
+{
+    ENTERTAINMENT,
+    EDUCATION,       
+    SPORTS,            
+    TRAVEL,                
+    OTHER,                 
+}
