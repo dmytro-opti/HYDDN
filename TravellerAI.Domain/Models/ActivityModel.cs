1c3fc00da71b17d2b47bac54a252435dd6c52573
yaroslavkhmil <yaroslavhmel2008@gmail.com>
2026-05-09 09:30:58 +0300

    add fields to ActivityModel

--- TravellerAI.Domain/Models/ActivityModel.cs
+++ TravellerAI.Domain/Models/ActivityModel.cs
@@ -1,6 +1,18 @@
+using TravellerAI.Domain.Enums;
+
 namespace TravellerAI.Domain.Models;
 
 public class ActivityModel
 {
-    
+    public string Name { get; set; }
+    public ActivityType Type { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public LocationModel Location { get; set; }
+    public JourneyStatus Status { get; set; }
+    public decimal Rating { get; set; }
+    public string ImageUrl { get; set; }
+    public string VideoUrl { get; set; }
+    public ReviewModel Review { get; set; }
+    public PeriodModel Period { get; set; }
 }
