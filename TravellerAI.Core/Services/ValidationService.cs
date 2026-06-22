e6be48f1beb3cfb41dafa9b2225cdf2f2ef14972
diana <diana.kov302@gmail.com>
2026-06-22 13:49:01 +0300

    Add Validation interfaces and Services

---
+++ TravellerAI.Core/Services/ValidationService.cs
@@ -0,0 +1,41 @@
+using TravellerAI.Core.Interfaces;
+using TravellerAI.Domain.Models;
+using TravellerAI.Domain.ViewModels.Requests;
+
+namespace TravellerAI.Core.Services;
+
+public class ValidationService : IValidationService
+{
+    public Task<bool> ValidateEmail(LoginUserModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ValidatePassword(LoginUserModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ValidateDate(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+   
+    public Task<bool> ValidateAccessibility()
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> ValidateBirthDate(UserInfoModel user)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> CheckString(string stringToCheck)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CheckInt(int intToCheck)
+    {
+        throw new NotImplementedException();
+    }
+}
