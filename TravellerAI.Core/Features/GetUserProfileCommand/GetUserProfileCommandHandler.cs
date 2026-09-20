81893340cb0bc2045939d4ad74bf38c52bcc1828
yaroslavkhmil <yaroslavhmel2008@gmail.com>
2026-09-20 12:41:12 +0300

    add Exceptions

--- TravellerAI.Core/Features/GetUserProfileCommand/GetUserProfileCommandHandler.cs
+++ TravellerAI.Core/Features/GetUserProfileCommand/GetUserProfileCommandHandler.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using TravellerAI.Core.Exceptions;
 using TravellerAI.Core.Features.BuildJourneyCommand;
 using TravellerAI.Core.Interfaces;
 using TravellerAI.Domain.Models;
@@ -18,7 +19,7 @@ public class GetUserProfileCommandHandler : IRequestHandler<GetUserProfileComman
         var userInfo = await _userService.GetUserInfoAsync(command.UserId);
         if (userInfo == null)
         {
-            throw new Exception($"User with ID {command.UserId} not found.");
+            throw new BadRequestException($"User with ID {command.UserId} not found.");
         }
 
         return userInfo;
