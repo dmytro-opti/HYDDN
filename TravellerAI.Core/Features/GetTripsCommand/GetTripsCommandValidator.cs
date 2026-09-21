f89d642f04441e0a912c44d22a2d01a8c6caba5f
yaroslavkhmil <yaroslavhmel2008@gmail.com>
2026-09-21 17:45:18 +0300

    refact command

--- TravellerAI.Core/Features/GetTripsCommand/GetTripsCommandValidator.cs
+++ TravellerAI.Core/Features/GetTripsCommand/GetTripsCommandValidator.cs
@@ -4,5 +4,9 @@ namespace TravellerAI.Core.Features.GetTripsCommand;
 
 public class GetTripsCommandValidator : AbstractValidator<GetTripsCommand>
 {
-    
+    public GetTripsCommandValidator()
+    {
+        RuleFor(input => input.UserId)
+            .NotEmpty().WithMessage("UserId cannot be null");
+    }
 }
